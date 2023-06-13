56ce3158161e00539c085d7ee311befcefd0269f
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-13 16:35:23 +0100

    code that resets the order table after every test

---
+++ spec/order_repository_spec.rb
@@ -0,0 +1,13 @@
+def reset_orders_table
+    seed_sql = File.read('spec/seeds_orders.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'orders' })
+    connection.exec(seed_sql)
+  end
+  
+  describe orderRepository do
+    before(:each) do 
+      reset_orders_table
+    end
+  
+    # (your tests will go here).
+end
