f9123c9f4ea8899cf426586e16ab1cd231e12b7d
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-13 16:20:34 +0100

    added more inserts into the tables

--- spec/seeds_order_items.sql
+++ spec/seeds_order_items.sql
@@ -2,8 +2,12 @@ TRUNCATE TABLE orders, items CASCADE;
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 
-INSERT INTO orders (id, customer_name, date_ordered) VALUES (1, 'Test1', '2013-04-06');
-INSERT INTO orders (id, customer_name, date_ordered) VALUES (2, 'Test2', '2006-07-14');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (1, 'ItemTest1', '2013-04-06');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (2, 'ItemTest2', '2006-07-14');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (3, 'ItemTest3', '2015-02-04');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (4, 'ItemTest4', '2007-04-11');
 
-INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('Test1', 11.56, 3, 1);
-INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('Test2', 11.66, 2, 2);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest1', 11.56, 3, 1);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest2', 12.66, 2, 2);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest3', 13.56, 3, 3);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest4', 14.66, 2, 3);
