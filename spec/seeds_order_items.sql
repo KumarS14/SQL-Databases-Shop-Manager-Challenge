00b3192c6cbc48eb135e93cee0cd92d39573a03b
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-13 16:38:39 +0100

    fixed the insert values as I got them switched

--- spec/seeds_order_items.sql
+++ spec/seeds_order_items.sql
@@ -2,12 +2,12 @@ TRUNCATE TABLE orders, items CASCADE;
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 
-INSERT INTO orders (id, customer_name, date_ordered) VALUES (1, 'ItemTest1', '2013-04-06');
-INSERT INTO orders (id, customer_name, date_ordered) VALUES (2, 'ItemTest2', '2006-07-14');
-INSERT INTO orders (id, customer_name, date_ordered) VALUES (3, 'ItemTest3', '2015-02-04');
-INSERT INTO orders (id, customer_name, date_ordered) VALUES (4, 'ItemTest4', '2007-04-11');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (1, 'Customer_name1', '2013-04-06');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (2, 'Customer_name2', '2006-07-14');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (3, 'Customer_name3', '2015-02-04');
+INSERT INTO orders (id, customer_name, date_ordered) VALUES (4, 'Customer_name4', '2007-04-11');
 
-INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest1', 11.56, 3, 1);
-INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest2', 12.66, 2, 2);
-INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest3', 13.56, 3, 3);
-INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('OrderTest4', 14.66, 2, 3);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('Item_test1', 11.56, 3, 1);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('Item_test2', 12.66, 2, 2);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('Item_test3', 13.56, 3, 3);
+INSERT INTO items (item_name, unit_price, quantity, order_id) VALUES ('Item_test4', 14.66, 2, 3);
