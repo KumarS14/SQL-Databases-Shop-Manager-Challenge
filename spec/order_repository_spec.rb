5f84c060df4669195e2d268a88cd7b951790eddb
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-13 16:49:28 +0100

    added code blocks I had forgotten before

--- spec/order_repository_spec.rb
+++ spec/order_repository_spec.rb
@@ -1,7 +1,7 @@
 require 'order.rb'
 require 'order_repository.rb'
 def reset_orders_table
-    seed_sql = File.read('spec/seeds_orders.sql')
+    seed_sql = File.read('spec/seeds_order_items.sql')
     connection = PG.connect({ host: '127.0.0.1', dbname: 'order_items_test' })
     connection.exec(seed_sql)
   end
@@ -10,18 +10,19 @@ def reset_orders_table
     before(:each) do 
       reset_orders_table
     end
-  
+  it 'should test to see if the all method returns the orders' do
     repo = OrderRepository.new
 
     orders = repo.all
 
     expect(orders.length).to eq(4)
 
-    (orders[0].id).to eq(1)
-    (orders[0].customer_name).to eq("Customer_nam1")
-    (orders[0].date_ordered).to eq("2013-04-06")
+    expect(orders[0].id).to eq(1)
+    expect(orders[0].customer_name).to eq("Customer_name1")
+    expect(orders[0].date_ordered).to eq("2013-04-06")
 
-    (orders[1].id).to eq(2)
-    (orders[1].customer_name).to eq("Customer_name2")
-    (orders[1].date_ordered).to eq('2006-07-14')
+    expect(orders[1].id).to eq(2)
+    expect(orders[1].customer_name).to eq("Customer_name2")
+    expect(orders[1].date_ordered).to eq('2006-07-14')
+  end
 end
