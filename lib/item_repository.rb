e9467cb8d51337e71eb4e3a374b71dbcdd5fc436
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-14 11:24:08 +0100

    item repo with all method that returns all items

---
+++ lib/item_repository.rb
@@ -0,0 +1,19 @@
+class ItemRepository
+    def all
+        sql = 'SELECT item_name, unit_price, quantity, order_id FROM items;'
+        result_set = DatabaseConnection.exec_params(sql,[])
+        items = []
+        result_set.each do |row|
+            item = Item.new
+            item.item_name = row['item_name']
+            item.unit_price = row['unit_price'].to_f
+            item.quantity = row['quantity'].to_i
+            item.order_id = row['order_id'].to_i
+            items << item
+        end
+        return items
+
+    end
+
+
+end
