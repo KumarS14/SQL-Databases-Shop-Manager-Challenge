32d82535e1b1fcf6064e1fe4d164a0bc23e1e83a
KumarS14 <sonjaykumar1404@gmail.com>
2023-06-14 11:48:53 +0100

    find method that returns a specifc id via id

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -13,8 +13,20 @@ class ItemRepository
             items << item
         end
         return items
-
     end
+    def find(id)
+        sql = 'SELECT id, item_name, unit_price, quantity, order_id from items WHERE id = $1;'
+        result_set =  DatabaseConnection.exec_params(sql,[id])
+        result = result_set[0]
+        item = Item.new
+        item.id = result['id'].to_i
+        item.item_name = result['item_name']
+        item.unit_price = result['unit_price'].to_f
+        item.quantity = result['quantity'].to_i
+        item.order_id = result['order_id'].to_i
+        return item
+    end
+
 
 
 end
